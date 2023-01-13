34b18378b250aea46505ae4f08bad111594d0729
evilbinary <rootntsd@gmail.com>
2023-01-14 00:56:31 +0800

    ref test

--- test/test-fork.c
+++ test/test-fork.c
@@ -3,15 +3,22 @@
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "fcntl.h"
 
 #include "cmocka.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/ioctl.h"
+#include "types.h"
+#include "unistd.h"
 
-int g_int = 1;  //数据段的全局变量
+#define IOC_PTY_MAGIC 'p'
+
+#define IOC_SLAVE _IOW(IOC_PTY_MAGIC, 1, int)
+#define IOC_READ_OFFSET _IOW(IOC_PTY_MAGIC, 3, int)
+#define IOC_WRITE_OFFSET _IOW(IOC_PTY_MAGIC, 4, int)
+
+int g_int = 1;  // 数据段的全局变量
 
 /**
 [PARENT] wait child exit
@@ -20,14 +27,14 @@ int g_int = 1;  //数据段的全局变量
 [PARENT] child have exit
 [PARENT] g_int = 1
 [PARENT] local_int = 1
-[PARENT] malloc_int = 1 
+[PARENT] malloc_int = 1
 */
 int test_fork() {
   printf("hello,fork\n");
   assert_int_equal(g_int, 1);
 
-  int local_int = 1;                      //栈上的局部变量
-  int* malloc_int = malloc(sizeof(int));  //通过malloc动态分配在堆上的变量
+  int local_int = 1;                      // 栈上的局部变量
+  int* malloc_int = malloc(sizeof(int));  // 通过malloc动态分配在堆上的变量
   *malloc_int = 1;
   pid_t pid = fork();
   if (pid == 0) /*子进程*/
@@ -63,9 +70,185 @@ int test_fork() {
   return 0;
 }
 
+void test_fork2() {
+  printf("fork\n");
+  u32 i = 0;
+  pid_t fpid = fork();
+  printf("fork end %d\n", fpid);
+  if (fpid == 0) {
+    printf("child %d %d %d\n", getppid(), getpid(), fpid);
+  } else {
+    printf("parent %d %d %d\n", getppid(), getpid(), fpid);
+  }
+  for (int i = 0; i < 10; i++) {
+    printf("ppid:%d pid:%d count:%d\n", getppid(), getpid(), i++);
+    // getpid();
+    // syscall0(505);
+  }
+  printf("\n");
+}
+
+void test_read_write() {
+  printf("fork\n");
+  u32 fd_ptm, fd_pts;
+  fd_ptm = open("/dev/ptm", O_RDONLY);
+  if (fd_ptm < 0) {
+    printf("error get ptm \n");
+  }
+  u32 i = 0;
+  pid_t fpid = fork();
+  printf("fork end %d\n", fpid);
+  if (fpid == 0) {
+    printf("child %d %d %d\n", getppid(), getpid(), fpid);
+    u32 pts = ioctl(fd_ptm, IOC_SLAVE);
+    char buf[20];
+    sprintf(buf, "/dev/pts/%d", pts);
+    fd_pts = open(buf, O_RDONLY);
+    if (fd_pts < 0) {
+      printf("error get pts \n");
+    }
+    int j = 0;
+    int pid = getpid();
+    for (;;) {
+      char buf2[64];
+      int len = 1;
+      memset(buf2, 0, len);
+      read(fd_pts, buf2, len);
+      printf("child pid:%d j:%d read from ptm: %s\n", pid, j++, buf2);
+    }
+  } else {
+    printf("parent %d %d %d\n", getppid(), getpid(), fpid);
+    int j = 0;
+    int pid = getpid();
+    for (;;) {
+      char* buf2 = "abcdef";
+      int len = strlen(buf2);
+      if (j < 2 || j > 1000000) {
+        printf("parent pid:%d j:%d write ptm: %s\n", pid, j, buf2);
+        write(fd_ptm, buf2, len);
+      }
+      j++;
+    }
+  }
+  for (int i = 0; i < 1000000; i++) {
+    printf("pid:%d count:%d\n", getpid(), i++);
+    // getpid();
+  }
+}
+
+void test_dup_pty() {
+  printf("test dup pty\n");
+  u32 fd_ptm, fd_pts;
+  fd_ptm = open("/dev/ptm", "r");
+  if (fd_ptm < 0) {
+    printf("error get ptm \n");
+  }
+  printf("test dup pty start fork\n");
+  u32 i = 0;
+  pid_t fpid = fork();
+  printf("fork end %d\n", fpid);
+  if (fpid == 0) {
+    printf("child ppid:%d pid:%d fork ret:%d\n", getppid(), getpid(), fpid);
+    u32 pts = ioctl(fd_ptm, IOC_SLAVE);
+    char buf[20];
+    sprintf(buf, "/dev/pts/%d", pts);
+    fd_pts = open(buf, "r");
+    if (fd_pts < 0) {
+      printf("error get pts \n");
+    }
+    printf(" salve pts:%d\n", fd_pts);
+    int j = 0;
+    int pid = getpid();
+    for (;;) {
+      char buf2[64];
+      int len = 1;
+      memset(buf2, 0, len);
+      int ret = read(fd_pts, buf2, len);
+      printf("pid:%d %d read from ptm: %s ret:%d\n", pid, j++, buf2, ret);
+    }
+  } else {
+    printf("parent ppid:%d pid:%d fork ret:%d\n", getppid(), getpid(), fpid);
+    int j = 0;
+    int pid = getpid();
+    for (;;) {
+      char* buf2 = "abcdef";
+      int len = strlen(buf2);
+      if (j < 3 || j > 10000000) {
+        printf("pid:%d %d write ptm: %s ", pid, j, buf2);
+        u32 ret = write(fd_ptm, buf2, len);
+        printf("   ret=%d\n");
+      }
+      j++;
+    }
+  }
+  for (int i = 0; i < 10; i++) {
+    printf("pid:%d count:%d\n", getpid(), i++);
+    // getpid();
+  }
+}
+
+void test_pipe() {
+  pid_t pid;
+  char buf[1024];
+  int fd[2];
+  char* p = "test for pipe\n";
+
+  if (pipe(fd) == -1) printf("pipe error\n");
+  printf(" fd0 %d fd1 %d\n", fd[0], fd[1]);
+  pid = fork();
+  if (pid < 0) {
+    printf("fork err pid %d\n", getpid());
+  } else if (pid == 0) {  // child
+    printf("child pid %d\n", getpid());
+    close(fd[1]);
+    printf("start read\n");
+    int len = read(fd[0], buf, strlen(p));
+    printf("child read====>%s ret:%d\n", buf, len);
+
+    memset(buf, 0, 1024);
+    len = read(fd[0], buf, strlen(p));
+    printf("child read2====>%s ret:%d\n", buf, len);
+
+    // write(STDOUT_FILENO, buf, len);
+    close(fd[0]);
+  } else {  // parent
+    printf("parent pid %d\n", getpid());
+    close(fd[0]);
+    for (int i = 0; i < 1000; i++)
+      ;
+    write(fd[1], p, strlen(p));
+    for (int i = 0; i < 1000; i++)
+      ;
+    write(fd[1], "ABCDEF", strlen(p));
+    close(fd[1]);
+  }
+  for (int i = 0; i < 2000; i++) {
+    printf("%d=[%d]", getpid(), i);
+  }
+}
+
+void test_pc() {
+  // printf("fork\n");
+  // syscall0(12);
+  u32 i = 0;
+  pid_t fpid = fork();
+  printf("t:%d\n", fpid);
+  for (;;) {
+    printf("t:%d i=>%d\n", fpid, i++);
+  }
+}
+
 int main(int argc, char* argv[]) {
   const struct CMUnitTest tests[] = {
-      cmocka_unit_test(test_fork),
+      // cmocka_unit_test(test_fork),
+      // cmocka_unit_test(test_fork2),
+
+      // cmocka_unit_test(test_pipe), //have free bug?
+
+      cmocka_unit_test(test_dup_pty),  // fork reboot?
+
+      // cmocka_unit_test(test_read_write),
+
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
--- test/test.c
+++ test/test.c
@@ -23,6 +23,7 @@ void test_pty() {
   fd_ptm = open("/dev/ptm", "r");
   if (fd_ptm < 0) {
     printf("error get ptm \n");
+    assert_true(false);
   }
   u32 pts = ioctl(fd_ptm, IOC_SLAVE);
   char buf[20];
@@ -30,6 +31,7 @@ void test_pty() {
   fd_pts = open(buf, "r");
   if (fd_pts < 0) {
     printf("error get pts \n");
+    assert_true(false);
   }
   printf("ptm %d pts %d\n", fd_ptm, fd_pts);
 
@@ -39,187 +41,40 @@ void test_pty() {
   memset(buf2, 0, len);
   read(fd_pts, buf2, len);
   printf("read from ptm: %s\n", buf2);
-}
-
-void test_fork() {
-  printf("fork\n");
-  u32 i = 0;
-  pid_t fpid = fork();
-  printf("fork end %d\n", fpid);
-  if (fpid == 0) {
-    printf("child %d %d %d\n", getppid(), getpid(), fpid);
-  } else {
-    printf("parent %d %d %d\n", getppid(), getpid(), fpid);
-  }
-  for (int i = 0; i < 10; i++) {
-    printf("ppid:%d pid:%d count:%d\n", getppid(), getpid(), i++);
-    // getpid();
-    // syscall0(505);
-  }
-  printf("\n");
-}
-
-void test_read_write() {
-  printf("fork\n");
-  u32 fd_ptm, fd_pts;
-  fd_ptm = open("/dev/ptm", "r");
-  if (fd_ptm < 0) {
-    printf("error get ptm \n");
-  }
-  u32 i = 0;
-  pid_t fpid = fork();
-  printf("fork end %d\n", fpid);
-  if (fpid == 0) {
-    printf("child %d %d %d\n", getppid(), getpid(), fpid);
-    u32 pts = ioctl(fd_ptm, IOC_SLAVE);
-    char buf[20];
-    sprintf(buf, "/dev/pts/%d", pts);
-    fd_pts = open(buf, "r");
-    if (fd_pts < 0) {
-      printf("error get pts \n");
-    }
-    int j = 0;
-    int pid = getpid();
-    for (;;) {
-      char buf2[64];
-      int len = 1;
-      memset(buf2, 0, len);
-      read(fd_pts, buf2, len);
-      printf("child pid:%d j:%d read from ptm: %s\n", pid, j++, buf2);
-    }
-  } else {
-    printf("parent %d %d %d\n", getppid(), getpid(), fpid);
-    int j = 0;
-    int pid = getpid();
-    for (;;) {
-      char* buf2 = "abcdef";
-      int len = strlen(buf2);
-      if (j < 2 || j > 1000000) {
-        printf("parent pid:%d j:%d write ptm: %s\n", pid, j, buf2);
-        write(fd_ptm, buf2, len);
-      }
-      j++;
-    }
-  }
-  for (int i = 0; i < 1000000; i++) {
-    printf("pid:%d count:%d\n", getpid(), i++);
-    // getpid();
-  }
-}
-
-void test_dup_pty() {
-  printf("fork\n");
-  u32 fd_ptm, fd_pts;
-  fd_ptm = open("/dev/ptm", "r");
-  if (fd_ptm < 0) {
-    printf("error get ptm \n");
-  }
-  u32 i = 0;
-  pid_t fpid = fork();
-  printf("fork end %d\n", fpid);
-  if (fpid == 0) {
-    printf("child ppid:%d pid:%d fork ret:%d\n", getppid(), getpid(), fpid);
-    u32 pts = ioctl(fd_ptm, IOC_SLAVE);
-    char buf[20];
-    sprintf(buf, "/dev/pts/%d", pts);
-    fd_pts = open(buf, "r");
-    if (fd_pts < 0) {
-      printf("error get pts \n");
-    }
-    printf(" salve pts:%d\n", fd_pts);
-    int j = 0;
-    int pid = getpid();
-    for (;;) {
-      char buf2[64];
-      int len = 1;
-      memset(buf2, 0, len);
-      int ret = read(fd_pts, buf2, len);
-      printf("pid:%d %d read from ptm: %s ret:%d\n", pid, j++, buf2, ret);
-    }
-  } else {
-    printf("parent ppid:%d pid:%d fork ret:%d\n", getppid(), getpid(), fpid);
-    int j = 0;
-    int pid = getpid();
-    for (;;) {
-      char* buf2 = "abcdef";
-      int len = strlen(buf2);
-      if (j < 3 || j > 10000000) {
-        printf("pid:%d %d write ptm: %s ", pid, j, buf2);
-        u32 ret = write(fd_ptm, buf2, len);
-        printf("   ret=%d\n");
-      }
-      j++;
-    }
-  }
-  for (int i = 0; i < 10; i++) {
-    printf("pid:%d count:%d\n", getpid(), i++);
-    // getpid();
-  }
-}
-
-void test_pipe() {
-  pid_t pid;
-  char buf[1024];
-  int fd[2];
-  char* p = "test for pipe\n";
-
-  if (pipe(fd) == -1) printf("pipe error\n");
-  printf(" fd0 %d fd1 %d\n", fd[0], fd[1]);
-  pid = fork();
-  if (pid < 0) {
-    printf("fork err pid %d\n", getpid());
-  } else if (pid == 0) {  // child
-    printf("child pid %d\n", getpid());
-    close(fd[1]);
-    printf("start read\n");
-    int len = read(fd[0], buf, strlen(p));
-    printf("child read====>%s ret:%d\n", buf, len);
-
-    memset(buf, 0, 1024);
-    len = read(fd[0], buf, strlen(p));
-    printf("child read2====>%s ret:%d\n", buf, len);
-
-    // write(STDOUT_FILENO, buf, len);
-    close(fd[0]);
-  } else {  // parent
-    printf("parent pid %d\n", getpid());
-    close(fd[0]);
-    for (int i = 0; i < 10000000; i++)
-      ;
-    write(fd[1], p, strlen(p));
-    for (int i = 0; i < 10000000; i++)
-      ;
-    write(fd[1], "ABCDEF", strlen(p));
-    close(fd[1]);
-  }
-  for (;;) {
-    printf("%d ", getpid());
-  }
+  assert_string_equal(buf2, "abcdef");
 }
 
 void test_dup() {
   FILE* fp = fopen("/testdup.txt", "w");
   printf("open fp %x\n", fp);
+  assert_non_null(fp);
   int fno = fileno(fp);
   fno = STDOUT_FILENO;
 
   int fd = dup(fno);
   printf("dup fd %d\n", fd);
+  assert_true(fp > 0);
 
   FILE* nfp = fdopen(fd, "w");
   fprintf(nfp, "test dup\n");
+
+  assert_non_null(nfp);
+
   close(fd);
 }
 
 void test_dup2() {
   int fd;
   fd = open("/testdup2.txt", "w");
+  assert_true(fd > 0);
+
   if (fd < 0) {
     printf("open error\n");
     exit(-1);
   }
   if (dup2(fd, STDOUT_FILENO) < 0) {
     printf("err in dup2\n");
+    assert_false(true);
   }
   printf("test str\n");
   close(fd);
@@ -240,20 +95,10 @@ void test_malloc_free() {
     void* p = malloc(1024 * 2);
     if (p == NULL) {
       printf("malloc error\n");
+      assert_non_null(p);
     } else {
       free(p);
     }
-  }
-}
-
-void test_pc() {
-  // printf("fork\n");
-  // syscall0(12);
-  u32 i = 0;
-  pid_t fpid = fork();
-  printf("t:%d\n", fpid);
-  for (;;) {
-    printf("t:%d i=>%d\n", fpid, i++);
   }
 }
 
@@ -268,35 +113,32 @@ void test_scanf() {
   int i = 0;
   scanf("%d", &i);
   printf("i=%d\n", i);
+
+  assert_true(i == 0);
 }
 
 void test_getcwd() {
   char buf[256] = {0};
   char* test = getcwd(buf, 20);
   printf("pwd=>%s\n", buf);
-  chdir("/haha");
-
+  int ret = chdir("/haha");
   test = getcwd(buf, 20);
   printf("pwd=>%s\n", buf);
+  if (ret == 0) {
+    assert_string_equal(buf, "/haha");
+  } else {
+    assert_string_equal(buf, "/");
+  }
 }
 
 int main(int argc, char* argv[]) {
   printf(buf);
-  // test_pc();
-  // syscall0(12);
-  // test_fork();
-  // test_pty();
-  // test_dup();
-  // test_dup2();
-  // test_pipe();
-  // test_exec();
 
   const struct CMUnitTest tests[] = {
-      cmocka_unit_test(test_getcwd),
-      cmocka_unit_test(test_scanf),      cmocka_unit_test(test_malloc_free),
-      cmocka_unit_test(test_read_write), cmocka_unit_test(test_dup_pty),
-      cmocka_unit_test(test_dup_pty),
-      cmocka_unit_test(test_syscall),
+      cmocka_unit_test(test_getcwd),      cmocka_unit_test(test_scanf),
+      cmocka_unit_test(test_malloc_free), cmocka_unit_test(test_syscall),
+      cmocka_unit_test(test_dup),         cmocka_unit_test(test_dup2),
+      cmocka_unit_test(test_pty),         cmocka_unit_test(test_exec),//fix me
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
