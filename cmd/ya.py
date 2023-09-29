d074aacc4ff93bc93546f3102db875100f2d0c57
evilbinary <rootntsd@gmail.com>
2023-09-29 20:27:18 +0800

    add build

---
+++ cmd/ya.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+# *******************************************************************
+# * Copyright 2021-present evilbinary
+# * 作者: evilbinary on 01/01/20
+# * 邮箱: rootdebug@163.com
+# ********************************************************************
+target("cat")
+set_type("cli")
+
+add_files("cat.c") 
+
+target("date")
+set_type("cli")
+add_files("date.c") 
+
+target("echo")
+set_type("cli")
+add_files("echo.c") 
+
+target("hexdump")
+set_type("cli")
+add_files("hexdump.c") 
+
+
+target("kill")
+set_type("cli")
+add_files("kill.c") 
+
+target("ls")
+set_type("cli")
+add_files("ls.c") 
+
+target("shell")
+set_type("cli")
+add_files("shell.c") 
+
+
+target("touch")
+set_type("cli")
+add_files("touch.c") 
---
+++ ya.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+# *******************************************************************
+# * Copyright 2021-present evilbinary
+# * 作者: evilbinary on 01/01/20
+# * 邮箱: rootdebug@163.com
+# ********************************************************************
+if has_config("app"):
+    for v in get_config('apps'):
+        includes("./"+v+"/ya.py")
+
+if has_config("cpp-apps"): 
+    for v in get_config('cpp_apps'):
+        includes("./"+v+"/ya.py")
