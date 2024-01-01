bb7deb59f7b3d39574aaf049e1854433c3890a6d
evilbinary <rootntsd@gmail.com>
2024-01-01 11:36:48 +0800

    fix build

--- gnuboy/ya.py
+++ gnuboy/ya.py
@@ -1,7 +1,7 @@
 target("gnuboy")
 set_type("cli")
 
-add_deps("sdl2","sdl2-image","zlib","gui")
+add_deps("sdl2","sdl2-image","gui")
 
 add_files(
     'src/*.c',
@@ -13,8 +13,8 @@ add_files(
 
 add_includedirs(
     'sys/sdl2/',
-    'include',
-    '.'
+    './include',
+    '.',
 )
 add_cflags('-DIS_LITTLE_ENDIAN  -DIS_LINUX -D_GNU_SOURCE=1 -D_REENTRANT ')
 
