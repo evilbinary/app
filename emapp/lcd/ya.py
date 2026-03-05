e1d208a6a5d9554c9e5a9def75538f9ecc01c272
evilbinary <rootntsd@gmail.com>
2026-03-06 00:18:22 +0800

    refactor: restructure boot file logic and link config

--- emapp/lcd/ya.py
+++ emapp/lcd/ya.py
@@ -26,11 +26,23 @@ arch=get_arch()
 arch_type=get_arch_type()
 plat=get_plat()
 
+print("DEBUG: arch=%s, arch_type=%s, plat=%s" % (arch, arch_type, plat))
+
 add_rules("kernel-objcopy")
+
+# ARM platforms always need boot startup for isr_vector
+if arch_type == 'arm':
+    print("DEBUG: Adding boot files for ARM")
+    add_files(
+            '../../../boot/'+arch_type+'/boot-'+arch+'.s',
+            '../../../boot/'+arch_type+'/init-'+arch+'.c',
+            )
 
 if has_config('single-kernel'):
     add_deps('boot-init.elf')
     add_defines('SINGLE_KERNEL')
+    print("Building single kernel",arch_type)
+
     if arch_type in['riscv','xtensa']:
         add_files(
                 '../../boot/'+arch_type+'/boot.s',
@@ -39,11 +51,6 @@ if has_config('single-kernel'):
     elif arch_type in['x86']:
         add_files(
                 '../../boot/'+arch_type+'/init.c',
-                )
-    else:
-        add_files(
-                '../../boot/'+arch_type+'/boot-'+arch+'.s',
-                '../../boot/'+arch_type+'/init-'+arch+'.c',
                 )
 
 
@@ -54,3 +61,29 @@ add_includedirs(
     '../',
     '../libs/include/archcommon'
 )
+
+def_arch=arch.replace("-", "_").upper()
+
+
+def_arch_type=arch_type.replace( "-", "_").upper()
+
+add_defines(def_arch)
+add_defines(def_arch_type)
+
+if arch_type=='general' and sys.platform in ['darwin']:
+    add_ldflags('-arch    i386 ')
+    pass
+else:
+    add_ldflags("-T"+path.join(os.scriptdir(), "../../xlinker/link-{plat}.ld"),  force = True)
+
+
+add_rules("kernel-objcopy")
+
+
+target('boot-config')
+# add_deps('kernel.elf')
+
+add_files(
+    "{buildir}/kernel"
+)
+add_rules("kernel-gen")
